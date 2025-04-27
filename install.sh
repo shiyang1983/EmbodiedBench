83102bc3288f1c9d9978cdff3904fbde29fc2f7e
yyshi <shiyang1983@gmail.com>
2025-04-27 11:25:25 -0700

    add proxy

--- install.sh
+++ install.sh
@@ -1,62 +1,70 @@
 #!/bin/bash
+
+
 source "$(conda info --base)/etc/profile.d/conda.sh"
 export EMBODIED_BENCH_ROOT=$(pwd)
 
+shopt -s expand_aliases
+
+alias with-proxy='HTTPS_PROXY=http://fwdproxy:8080 HTTP_PROXY=http://fwdproxy:8080 FTP_PROXY=http://fwdproxy:8080 https_proxy=http://fwdproxy:8080 http_proxy=http://fwdproxy:8080 ftp_proxy=http://fwdproxy:8080 http_no_proxy='\''\'\'\''*.facebook.com|*.tfbnw.net|*.fb.com'\''\'\'
+
+
+
 
 # # Environment for ```Habitat and Alfred```
-conda env create -f conda_envs/environment.yaml 
+with-proxy conda env create -f conda_envs/environment.yaml 
 conda activate embench
-pip install -e .
+with-proxy pip install -e .
 
 # Environment for ```EB-Navigation```
-conda env create -f conda_envs/environment_eb-nav.yaml 
+with-proxy conda env create -f conda_envs/environment_eb-nav.yaml 
 conda activate embench_nav
-pip install -e .
+with-proxy pip install -e .
 
 # Environment for ```EB-Manipulation```
-conda env create -f conda_envs/environment_eb-man.yaml 
+with-proxy conda env create -f conda_envs/environment_eb-man.yaml 
 conda activate embench_man
-pip install -e .
+with-proxy pip install -e .
 
 # Install Git LFS
-git lfs install
-git lfs pull
+with-proxy git lfs install
+with-proxy git lfs pull
 
 # Install EB-ALFRED
 conda activate embench
-git clone https://huggingface.co/datasets/EmbodiedBench/EB-ALFRED
+with-proxy git clone https://huggingface.co/datasets/EmbodiedBench/EB-ALFRED
 mv EB-ALFRED embodiedbench/envs/eb_alfred/data/json_2.1.0
 
 # Install EB-Habitat
 conda activate embench
-conda install -y habitat-sim==0.3.0 withbullet  headless -c conda-forge -c aihabitat
-git clone -b 'v0.3.0' --depth 1 https://github.com/facebookresearch/habitat-lab.git ./habitat-lab
+with-proxy conda install -y habitat-sim==0.3.0 withbullet  headless -c conda-forge -c aihabitat
+with-proxy git clone -b 'v0.3.0' --depth 1 https://github.com/facebookresearch/habitat-lab.git ./habitat-lab
 cd ./habitat-lab
-pip install -e habitat-lab
+with-proxy pip install -e habitat-lab
 cd ..
-conda install -y -c conda-forge git-lfs
-python -m habitat_sim.utils.datasets_download --uids rearrange_task_assets
+with-proxy conda install -y -c conda-forge git-lfs
+with-proxy python -m habitat_sim.utils.datasets_download --uids rearrange_task_assets
 mv data embodiedbench/envs/eb_habitat
 
 # Install EB-Manipulation
 conda activate embench_man
 cd embodiedbench/envs/eb_manipulation
-wget https://downloads.coppeliarobotics.com/V4_1_0/CoppeliaSim_Pro_V4_1_0_Ubuntu20_04.tar.xz
+with-proxy wget https://downloads.coppeliarobotics.com/V4_1_0/CoppeliaSim_Pro_V4_1_0_Ubuntu20_04.tar.xz
 tar -xf CoppeliaSim_Pro_V4_1_0_Ubuntu20_04.tar.xz
 rm CoppeliaSim_Pro_V4_1_0_Ubuntu20_04.tar.xz
 mv CoppeliaSim_Pro_V4_1_0_Ubuntu20_04/ $EMBODIED_BENCH_ROOT
 export COPPELIASIM_ROOT=$EMBODIED_BENCH_ROOT/CoppeliaSim_Pro_V4_1_0_Ubuntu20_04
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$COPPELIASIM_ROOT
 export QT_QPA_PLATFORM_PLUGIN_PATH=$COPPELIASIM_ROOT
-git clone https://github.com/stepjam/PyRep.git
+with-proxy git clone https://github.com/stepjam/PyRep.git
 cd PyRep
-pip install -r requirements.txt
-pip install -e .
+with-proxy pip install -r requirements.txt
+with-proxy pip install -e .
 cd ..
-pip install -r requirements.txt
-pip install -e .
+with-proxy pip install -r requirements.txt
+with-proxy pip install -e .
 cp ./simAddOnScript_PyRep.lua $COPPELIASIM_ROOT
-git clone https://huggingface.co/datasets/EmbodiedBench/EB-Manipulation
+with-proxy git clone https://huggingface.co/datasets/EmbodiedBench/EB-Manipulation
 mv EB-Manipulation/data/ ./
 rm -rf EB-Manipulation/
 cd ../../..
